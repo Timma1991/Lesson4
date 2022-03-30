738f33be0a77400508f3bc68fd97053ea1910970
timmi <Tima1>
2022-03-30 20:21:59 +0300

    finished working on lesson4

---
+++ Main1/Main1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Main1
+{
+    class Program
+    {
+        //Написать метод GetFullName(string firstName, string lastName, string patronymic),
+        //принимающий на вход ФИО в разных аргументах и возвращающий
+        //объединённую строку с ФИО.Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(GetFullName("Тимофей","Лебедев","Викторович"));
+            Console.WriteLine(GetFullName("Тимофей", "Лебедев", "Викторович"));
+            Console.WriteLine(GetFullName("Тим", "Ле", "Ви"));
+
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{lastName} {firstName} {patronymic}";
+        }
+
+
+      
+       
+    }
+}
---
+++ Metod2/Metod2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Metod2
+{//Написать программу,
+ //принимающую на вход строку —
+ //набор чисел, разделенных пробелом,
+ //и возвращающую число — сумму всех чисел
+ //в строке. Ввести данные с клавиатуры и
+ //вывести результат на экран.
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = RecieveData();
+            int result = Calculate(input);
+            Console.WriteLine($"result is: {result}");
+        }
+
+        private static string RecieveData()
+        {
+            Console.WriteLine("Введите строку: ");
+            return Console.ReadLine();
+        }
+
+        
+        private static int Calculate(string someText) // 15 34 53 1
+        {
+            string currentNumber = ""; //
+            int result = 0; //102
+
+            for(int i = 0; i < someText.Length; i++)
+            {
+                if (char.IsDigit(someText[i]))
+                {
+                    currentNumber += someText[i];
+                }
+
+                if(someText[i] == ' ' || i == someText.Length - 1)
+                {
+                    result = result + int.Parse(currentNumber); // result = 49 + int.Parse("53") = result = 49 + 53 => result = 102
+                    currentNumber = "";
+                }
+            }
+            return result;
+        }
+    }
+}
+
+
+
+
